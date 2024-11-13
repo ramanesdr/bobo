7ffea480664e7c01f99f2d0065c78b9127f7ddce
abdourahamane saley haougui <ashaougui@gmail.com>
2024-11-14 00:46:57 +0100

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quotes = {
   " Didier ": "On la connaît la logique de l'équipe de France. Quand vous gagnez, c'est la France Black-Blanc-Beur qui triomphe puis dès que vous perdez, c'est : ça suffit y'a trop d'étranger dans l'équipe.",
-  "- Bobo1 ": "Vive le mariage homosexuel ! Mais je suis contre l'adoption...",
+  "- Bobo1 ": "Vive le mariage homosexuel ! Mais je suis contre l'adoption....",
   "- Bobo2 ": "Il est tombé comme du n'importe quoi, on dirait la girafe qui s'emmêle les pattes.",
   "- Bobo3 ": "L'oiseau sur le baobab ne doit pas oublier qu'il a porté des lunettes ",
   "- Bobo4 ": "La France nous a souvent volé nos richesses... vous allez aussi nous voler nos footballeur ?",
